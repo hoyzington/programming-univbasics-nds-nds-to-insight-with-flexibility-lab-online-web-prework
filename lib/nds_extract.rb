98dfb62d67bb505d1b31e8c543e7cf4a029df4b3
Scott Hoisington <hoyzington@gmail.com>
2019-11-07 23:11:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,20 +35,17 @@ end
 
 def movies_with_director_key(name, movies_collection)
   
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
+  # GOAL:   For each Hash in an Array (movies_collection), provide a collection
+  #         of movies and a directors name to the movie_with_director_name method
+  #         and accumulate the returned Array of movies into a new Array that's
+  #         returned by this method.
   #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
+  # INPUT:  (name), a director's name, and (movies_collection), an AoH where
+  #         each Hash represents a movie
   #
-  # RETURN:
-  #
-  # AoH where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  # RETURN: AoH where each Hash represents a movie; however, they should all have
+  #         a :director_name key. This addition can be done by using the provided
+  #         movie_with_director_name method
   
 end
 
@@ -57,17 +54,14 @@ end
 
 def gross_per_studio(collection)
   
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
+  # GOAL:   Given an AoH where each Hash is a movie, return a Hash that includes
+  #         includes the total worldwide_gross of all the movies from each studio.
   #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
+  # INPUT:  (collection) AoH where each Hash represents a movie
   #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+  # RETURN: Hash where the keys are the studios and whe values are the sum of
+  #         all the worldwide_gross numbers for every movie in the input Hash
+  
 end
 
 
@@ -75,22 +69,19 @@ end
 
 def movies_with_directors_set(source)
   
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
+  # GOAL:   For each director, find their :movies Array and put it in a new Array
   #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
+  # INPUT:  (source) An AoH containing director info incl. :name and :movies
   #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  # RETURN: AoA containing all of a director's movies. Each movie will need
+  #         to have a :director_name key added to it.
+  
 end
 
 
 
 # ----------------    End of Your Code Region --------------------
-# Don't edit the following code! Make the methods above work with these 
+# Don't edit this code! See-saw to make the previous methods work with these 
 # You'll have to "see-saw" to get this to work!
 
 def studios_totals(nds)
