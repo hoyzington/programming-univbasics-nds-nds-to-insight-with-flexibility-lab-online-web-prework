3978eb1f035d4a3947c926296f7376e9513b2a7b
Scott Hoisington <hoyzington@gmail.com>
2019-11-08 03:48:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require 'directors_database'
       # 
 
 def movies_with_director_key(name, movies_collection)
+  updated_movies = []
   
   # GOAL:   For each Hash in an Array (movies_collection), provide a collection
   #         of movies and a directors name to the movie_with_director_name method
@@ -17,6 +18,7 @@ def movies_with_director_key(name, movies_collection)
   #         a :director_name key. This addition can be done by using the provided
   #         movie_with_director_name method
   
+  updated_movies
 end
 
 
