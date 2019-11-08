b2cf973d4842270607bb29e96eac9c274e7138aa
Scott Hoisington <hoyzington@gmail.com>
2019-11-08 05:08:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -65,25 +65,6 @@ def flatten_a_o_a(aoa)
 end
 
 
-      # A hash that includes :director_name as a key (INDIVIDUAL DIRECTOR)
-
-def movie_with_director_name(director_name, movie_data)
-  result = {}
-  i = 0
-  while i < x do
-    new_movie = { 
-      :title => movie_data[:title],
-      :worldwide_gross => movie_data[:worldwide_gross],
-      :release_year => movie_data[:release_year],
-      :studio => movie_data[:studio],
-      :director_name => director_name
-    }
-    i += 1
-  end
-  result
-end
-
-
       # A hash that includes :director_name as a key (ALL DIRECTORS)
 
 def movies_with_director_names(director_name, movie_data)
