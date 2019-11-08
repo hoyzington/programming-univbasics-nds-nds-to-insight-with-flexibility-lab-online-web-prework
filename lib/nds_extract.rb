8972457003de11cfb90927f5ba50230dc5973f14
Scott Hoisington <hoyzington@gmail.com>
2019-11-08 04:51:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -78,13 +78,7 @@ def movie_with_director_name(director_name, movie_data)
     i += 1
   end
   
-  new_movie = { 
-    :title => movie_data[:title],
-    :worldwide_gross => movie_data[:worldwide_gross],
-    :release_year => movie_data[:release_year],
-    :studio => movie_data[:studio],
-    :director_name => director_name
-  }
+  
   new_movie
 end
 
